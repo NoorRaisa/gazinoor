02b7304ca2e326434d554d6a0466e32942d63d1f
NoorRaisa <gazinoor01@gmail.com>
2026-01-19 13:38:14 +0600

    hamburger logic implemented

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Gazi Noor</title>
     <link rel="icon" href="images/favicon.png" type="image/png">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo filemtime('css/style.css'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="top-nav">
+<!-- <nav class="top-nav">
     <ul>
         <li style="color:black; font-style: italic;">Gazi Maliha Raisa Noor</li>
         <li><a href="index.php">About</a></li>
@@ -6,4 +6,25 @@
         <li><a href="publication.php">Publications</a></li>
         <li><a href="projects.php">Projects</a></li>
     </ul>
+</nav> -->
+<nav class="top-nav">
+    <input type="checkbox" id="nav-toggle" class="nav-toggle">
+    <label for="nav-toggle" class="nav-toggle-label">
+        <span></span>
+    </label>
+
+    <ul class="nav-links">
+        <li class="nav-brand" style="color:black; font-style: italic;">Gazi Maliha Raisa Noor</li>
+        <li><a href="index.php">About</a></li>
+        <li><a href="education.php">Education & Awards</a></li>
+        <li><a href="publication.php">Publications</a></li>
+        <li><a href="projects.php">Projects</a></li>
+    </ul>
 </nav>
+<script>
+    document.querySelectorAll('.nav-links a').forEach(link => {
+        link.addEventListener('click', () => {
+            document.getElementById('nav-toggle').checked = false;
+        });
+    });
+</script>
